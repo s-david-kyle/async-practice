4a595debcf91964d45e6bc8b8bc7dd6955c395b3
S. David Kyle <sdavidkyle@gmail.com>
2024-03-28 21:45:28 -0700

    Using threading with BlockingCollection mysteries

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace async;
+﻿using System.Collections.Concurrent;
+
+namespace async;
 
 class Program
 {
@@ -8,12 +10,39 @@ class Program
         for (int i = 0; i < 1000; i++)
         {
             int index = i; // Create a local variable and assign the value of i to it
-            ThreadPool.QueueUserWorkItem(delegate
+            MyThreadPool.QueueUserWorkItem(() =>
             {
                 Console.WriteLine("Hello, World! " + index); // Use the local variable instead of i
                 Thread.Sleep(10);
             });
         }
+
         Console.ReadLine();
     }
+}
+
+static class MyThreadPool
+{
+    public static readonly BlockingCollection<Action> s_workItems = new();
+    public static void QueueUserWorkItem(Action action) => s_workItems.Add(action);
+
+
+
+    static MyThreadPool()
+    {
+        for (int i = 0; i < Environment.ProcessorCount; i++)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Action action = s_workItems.Take();
+                    Console.WriteLine(s_workItems.Count);
+                    action();
+                }
+            })
+            { IsBackground = true }.Start();
+        }
+    }
+
 }
